bc7c869f0c91903ebca7baaa1256ef9549bb97cd
Luke Kiprop <luke.rono@student.moringaschool.com>
2025-03-04 14:39:02 +0300

    Complete lab

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// code your solution here
+// Define the saturdayFun function declaration
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+}
+
+// Define the mondayWork function expression
+const mondayWork = function(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`;
+};
+
+// Define the wrapAdjective function
+function wrapAdjective(wrapper = "*") {
+    return function(adjective = "special") {
+        return `You are ${wrapper}${adjective}${wrapper}!`;
+    };
+}
